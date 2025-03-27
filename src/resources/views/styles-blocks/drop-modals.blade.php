eba2541b54deb9871b6811d287a098564aa87cd2
Labrock <vladimirpeshkov5@yandex.ru>
2025-03-27 17:45:27 +0300

    add no livewire modal example

--- src/resources/views/styles-blocks/drop-modals.blade.php
+++ src/resources/views/styles-blocks/drop-modals.blade.php
@@ -7,16 +7,30 @@
             <div class="card-body">
                 <div class="row">
                     <div class="col w-1/3">
-                        <h4 class="text-2xl card-title">Dropdown</h4>
+                        <div class="space-y-indent">
+                            <div>
+                                <h4 class="text-2xl card-title">Dropdown</h4>
 
-                        <x-tt::dropdown>
-                            <x-slot name="text">Click</x-slot>
-                            <x-slot name="content">
-                                <div class="px-indent-half">
-                                    Hello
-                                </div>
-                            </x-slot>
-                        </x-tt::dropdown>
+                                <x-tt::dropdown>
+                                    <x-slot name="text">Click</x-slot>
+                                    <x-slot name="content">
+                                        <div class="px-indent-half">
+                                            Hello
+                                        </div>
+                                    </x-slot>
+                                </x-tt::dropdown>
+                            </div>
+                             <div>
+                                 <h4 class="text-2xl card-title">Modal without livewire</h4>
+                                 <button type="button" class="btn w-full btn-outline-primary" x-data @click="$dispatch('show-normal-modal')">
+                                     Click me
+                                 </button>
+                                 <x-tt::modal.aside direction="left" id="modalWithoutLivewire" event="show-normal-modal">
+                                     <x-slot name="title">Aside title</x-slot>
+                                     <x-slot name="content">Hello</x-slot>
+                                 </x-tt::modal.aside>
+                             </div>
+                        </div>
                     </div>
 
                     <div class="col w-2/3">
