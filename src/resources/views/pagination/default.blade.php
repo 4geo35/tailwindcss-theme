e2f4692a16449a94261700566e1a49cf25e95ec6
Labrock <vladimirpeshkov5@yandex.ru>
2025-08-13 18:51:40 +0300

    fix pagination and change h4 size

--- src/resources/views/pagination/default.blade.php
+++ src/resources/views/pagination/default.blade.php
@@ -10,7 +10,7 @@
                         </svg>
                     </span>
                 @else
-                    <a href="{{ $paginator->previousPageUrl() }}" rel="prev" class="inline-flex items-center px-1.5 py-1.5 hover:text-secondary">
+                    <a href="{{ $paginator->previousPageUrl() }}" rel="prev" class="inline-flex items-center px-1.5 py-1.5 hover:text-secondary cursor-pointer">
                         <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                             <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
                         </svg>
@@ -30,7 +30,7 @@
                             @if ($page == $paginator->currentPage())
                                 <span class="hidden md:inline-flex items-center justify-center min-w-[2rem] px-1.5 py-1.5 rounded-full bg-primary text-white">{{ $page }}</span>
                             @else
-                                <a href="{{ $url }}" class="hidden md:inline-flex items-center justify-center min-w-[2rem] px-1.5 py-1.5 hover:text-secondary">
+                                <a href="{{ $url }}" class="hidden md:inline-flex items-center justify-center min-w-[2rem] px-1.5 py-1.5 hover:text-secondary cursor-pointer">
                                     {{ $page }}
                                 </a>
                             @endif
@@ -40,7 +40,7 @@
 
                 {{-- Next Page Link --}}
                 @if ($paginator->hasMorePages())
-                    <a href="{{ $paginator->nextPageUrl() }}" rel="next" class="inline-flex items-center px-1.5 py-1.5 hover:text-secondary">
+                    <a href="{{ $paginator->nextPageUrl() }}" rel="next" class="inline-flex items-center px-1.5 py-1.5 hover:text-secondary cursor-pointer">
                         <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                             <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
                         </svg>
--- src/resources/views/pagination/web-live.blade.php
+++ src/resources/views/pagination/web-live.blade.php
@@ -24,7 +24,7 @@
                         </span>
                     @else
                         <button wire:click="previousPage('{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" wire:loading.attr="disabled" dusk="previousPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}.before"
-                                class="inline-flex items-center px-1.5 py-1.5 hover:text-secondary">
+                                class="inline-flex items-center px-1.5 py-1.5 hover:text-secondary cursor-pointer">
                             <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                                 <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
                             </svg>
@@ -46,7 +46,7 @@
                                         <span class="hidden md:inline-flex items-center justify-center min-w-[2rem] px-1.5 py-1.5 rounded-full bg-primary text-white">{{ $page }}</span>
                                     @else
                                         <button wire:click="gotoPage({{ $page }}, '{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}"
-                                                class="hidden md:inline-flex items-center justify-center min-w-[2rem] px-1.5 py-1.5 hover:text-secondary">
+                                                class="hidden md:inline-flex items-center justify-center min-w-[2rem] px-1.5 py-1.5 hover:text-secondary cursor-pointer">
                                             {{ $page }}
                                         </button>
                                     @endif
@@ -59,7 +59,7 @@
                         {{-- Next Page Link --}}
                         @if ($paginator->hasMorePages())
                             <button wire:click="nextPage('{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" dusk="nextPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}.after"
-                                    class="inline-flex items-center px-1.5 py-1.5 hover:text-secondary">
+                                    class="inline-flex items-center px-1.5 py-1.5 hover:text-secondary cursor-pointer">
                                 <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
                                 </svg>
