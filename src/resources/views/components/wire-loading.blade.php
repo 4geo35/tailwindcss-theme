537c9d3ece2fad97bf6c102349bb3a2936d589b1
Labrock <vladimirpeshkov5@yandex.ru>
2026-02-06 11:46:38 +0300

    fix loading

--- src/resources/views/components/wire-loading.blade.php
+++ src/resources/views/components/wire-loading.blade.php
@@ -1,4 +1,4 @@
-<div class="absolute top-0 left-0 w-full h-full bg-body/15 flex items-center justify-center">
+<div class="absolute top-0 left-0 w-full h-full bg-body/15 flex items-center justify-center" wire:loading>
     <div class="inline-block text-body duration-150 ease-in-out transition">
         <x-tt::ico.loading class="animate-[spin_2s_linear_infinite]" width="48" height="48" />
     </div>
