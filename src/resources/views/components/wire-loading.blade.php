2de26aa5ae8d718e66860d181be80f1f5b0a9850
Labrock <vladimirpeshkov5@yandex.ru>
2026-02-06 12:01:44 +0300

    change loading position

--- src/resources/views/components/wire-loading.blade.php
+++ src/resources/views/components/wire-loading.blade.php
@@ -1,5 +1,5 @@
-<div class="absolute top-0 left-0 w-full h-full bg-body/15 flex items-center justify-center" wire:loading>
-    <div class="inline-block text-body duration-150 ease-in-out transition">
+<div class="absolute top-0 left-0 bottom-0 right-0 bg-body/15 z-50 rounded-base overflow-hidden text-center" wire:loading>
+    <div class="flex items-center justify-center w-full h-full text-body duration-150 ease-in-out transition text-warning">
         <x-tt::ico.loading class="animate-[spin_2s_linear_infinite]" width="48" height="48" />
     </div>
 </div>
