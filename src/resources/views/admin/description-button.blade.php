42b263caa4c58a92bfb6391c26a2dc1a1d6efe23
Labrock <vladimirpeshkov5@yandex.ru>
2026-07-08 13:19:33 +0300

    change description text

--- src/resources/views/admin/description-button.blade.php
+++ src/resources/views/admin/description-button.blade.php
@@ -1,4 +1,5 @@
 @props(["id" => "infoHidden"])
-<button type="button" class="text-info hover:text-info-hover ml-indent-half" x-data="{}" x-on:click.prevent.stop="document.getElementById('{{ $id }}').click()">
+<button type="button" class="text-info hover:text-info-hover ml-indent-half cursor-pointer"
+        x-data="{}" x-on:click.prevent.stop="document.getElementById('{{ $id }}').click()">
     <x-tt::ico.question />
 </button>
--- src/resources/views/admin/description-info.blade.php
+++ src/resources/views/admin/description-info.blade.php
@@ -2,26 +2,26 @@
 <div class="h-0">
     <x-tt::dropdown class="top-0">
         <x-slot name="button">
-            <button type="button" id="{{ $id }}" class="hidden text-info hover:text-info-hover">
+            <button type="button" id="{{ $id }}" class="hidden text-info hover:text-info-hover cursor-pointer">
                 <x-tt::ico.question />
             </button>
         </x-slot>
         <x-slot name="content">
             <div class="text-sm p-indent-half space-y-indent-half">
                 <p>
-                    Для добавления параграфа добавить двойной перевод строки <span class="text-info">(Shift+Enter дважды)</span>
+                    Для добавления параграфа добавить двойной перевод строки <span class="text-info font-semibold">(Shift+Enter дважды)</span>
                 </p>
                 <p>
-                    Для перевода строки добавить два или больше пробела в конец строки и перевести строку <span class="text-info">(Shift+Enter)</span>
+                    Для перевода строки добавить два или больше пробела в конец строки и перевести строку <span class="text-info font-semibold">(Shift+Enter)</span>
                 </p>
                 <p>
-                    Для добавления жирного текста добавить <span class="text-info">**</span> до и после фрагмента текста
+                    Для добавления жирного текста добавить <span class="text-info font-semibold">**</span> до и после фрагмента текста
                 </p>
                 <p>
-                    Для добавления списка добавить <span class="text-info">-</span> с пробелом после, перед каждой строкой в списке
+                    Для добавления списка добавить <span class="text-info font-semibold">-</span>, либо <span class="text-info font-semibold">[0-9].</span>, с пробелом после, перед каждой строкой в списке
                 </p>
                 <p>
-                    Для добавления ссылки окружить текст ссылки <span class="text-info">[]</span>, после окружить ссылку <span class="text-info">()</span>. Пример: <span class="text-info">[Example link](https://ya.ru)</span>
+                    Для добавления ссылки окружить текст ссылки <span class="text-info font-semibold">[]</span>, после окружить ссылку <span class="text-info font-semibold">()</span>. Пример: <span class="text-info font-semibold">[Example link](https://ya.ru)</span>
                 </p>
             </div>
         </x-slot>
